a0716c7bfc28a43ee0028c7acc8deacaac67ab51
Adam Burns <adam@adamjohnburns.com>
2015-02-06 00:56:34 +1100

    Learning

--- Assets/Script01.cs
+++ Assets/Script01.cs
@@ -3,9 +3,34 @@ using System.Collections;
 
 public class Script01 : MonoBehaviour {
 
+	public GameObject gameObj;
+
+	public enum Status {
+		None,
+		Poison,
+		Sleep
+	}
+
 	// Use this for initialization
 	void Start () {
+		int x, y;
 
+		for (y = -5; y < 5; y++) {
+			for (x = -5; x < 5; x++) {
+				GameObject cube = GameObject.CreatePrimitive (PrimitiveType.Cube);
+				cube.AddComponent<Rigidbody>();
+				cube.transform.position = new Vector3(x, y, Random.Range (-5,5));
+				Debug.Log (x + " " + y + " " + 0);
+			}
+		}
+
+		for(y = -5; y < 5; y++) {
+			for(x = -5; x < 5; x++) {
+				Instantiate(gameObj, new Vector3(x, y + 10, Random.Range (-5,5)), Quaternion.identity);
+			}
+		}
+
+		Debug.Log("ran!");
 	}
 
 	// Update is called once per frame
