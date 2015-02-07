6d6d230e2251f8ae4e35432aeb19052db9aab0ad
Adam Burns <adam@adamjohnburns.com>
2015-02-07 12:03:33 +1100

    Added city generator

---
+++ Assets/CityBuilder.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CityBuilder : MonoBehaviour {
+
+	public GameObject cityBlock;
+	public Camera camera;
+
+	// Use this for initialization
+	void Start () {
+		for (var x = -25; x < 25; x++) {
+			for (var z = 5; z < 55; z++) {
+				Instantiate( cityBlock, 
+				            new Vector3(
+								x * 20, 
+								0, 
+								z * 20), 
+				            Quaternion.identity);
+			}
+		}
+	
+		Debug.Log(cityBlock.transform.localScale.z);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey ("")) {
+
+		}
+	}
+}
